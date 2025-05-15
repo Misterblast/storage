66a3a8bd2f3f43931704edb15c231e1b7881e41d
ghulammuzz <ghulamxiaomi@gmail.com>
2025-05-16 00:18:04 +0700

    feat: cors

--- utils/cors.go
+++ utils/cors.go
@@ -2,16 +2,20 @@ package utils
 
 import (
 	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Cors() fiber.Handler {
+	origins := strings.Split(os.Getenv("CORS_ORIGIN"), ",")
+	methods := os.Getenv("CORS_METHODS")
+
 	return cors.New(
 		cors.Config{
-			AllowOrigins: os.Getenv("CORS_ORIGIN"),
-			AllowMethods: os.Getenv("CORS_METHODS"),
+			AllowOrigins: strings.Join(origins, ","),
+			AllowMethods: methods,
 		},
 	)
 }
